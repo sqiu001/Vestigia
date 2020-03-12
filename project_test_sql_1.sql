7928a62db7c5e379c88c3a0d2dc5d1253e56a0ae
nalam004 <47127568+nalam004@users.noreply.github.com>
2020-03-12 17:06:09 -0400

    Update project_test_sql_1.sql

--- project_test_sql_1.sql
+++ project_test_sql_1.sql
@@ -3,88 +3,74 @@ CREATE DATABASE DATABASE_PROJECT;
 use DATABASE_PROJECT;
 
 --  create table user
-CREATE TABLE tb_user ( 
-	user_id INT AUTO_INCREMENT,
+CREATE TABLE user ( 
+	user_id INTEGER PRIMARY KEY AUTO_INCREMENT = 100,
 	first_name VARCHAR(50),
 	last_name VARCHAR(50),
-	email VARCHAR(100),
-     PRIMARY KEY (user_id)
-	);
- 
--- setting user_id auto increment from 100
-ALTER TABLE tb_user AUTO_INCREMENT = 100;
-
--- insert test data for user
-insert into tb_user (first_name, last_name, email) values 
-('YIFENG', 'Jin', 'www.111@111.com'),
-('Niharika', 'Alam', 'www.222@222.com'),
-('Sandy', 'Qiu', 'www.333@333.com'),
-('Silvija', 'Skemaite', 'www.444@444.com');
+	email VARCHAR(100)
+);
 
 -- create table profile
-create table tb_profile (
-	user_id INT auto_increment,
-    profile_display_name varchar (50),
-    profile_avatar bit default 1,       -- 0 means job seeker, 1 means HR 
-    primary key (user_id)
-    );
-
--- setting user_id auto increment from 100
-ALTER TABLE tb_profile AUTO_INCREMENT = 100;
-
--- insert test data for profile
-insert into tb_profile (profile_display_name, profile_avatar) values 
-('AAA', 0),
-('BBB', 1),
-('CCC', 0),
-('DDD', 1);
+CREATE TABLE profile (
+	user_id INTEGER PRIMARY KEY AUTO_INCREMENT = 100,
+	display_name VARCHAR(50),
+	avatar BIT default 1,       -- 0 means job seeker, 1 means HR 
+);
 
 -- create table follows
-create table tb_follows (
-	user_id int not null,
-    followed_user_id int not null,
-    primary key (followed_user_id)
-    );
+CREATE TABLE follows (
+	user_id INTEGER not null,
+	followed_user_id INTEGER PRIMARY KEY not null,
+);
     
 -- create table post
-create table tb_post (
-	post_id int,
-    post_job_id int,
-    user_id int not null,
-    post_time_stamp datetime,
-    primary key (post_id),
-    foreign key (user_id) references tb_user (user_id)
-	);
+CREATE TABLE post (
+	post_id INTEGER PRIMARY KEY,
+	job_id INTEGER,
+	user_id INTEGER not null,
+	time_stamp DATETIME,
+	foreign key (user_id) references user (user_id)
+);
 
 -- create table replies
-create table tb_replies (
-	reply_id int not null,
-    user_id int not null,
-    text varchar(5000),
-    reply_time_stamp datetime,
-    primary key (reply_id),
-    foreign key (user_id) references tb_user (user_id)
-	);
+CREATE TABLE replY (
+	reply_id INTEGER PRIMARY KEY not null,
+	post_id INTEGER,
+	user_id INTEGER not null,
+	text VARCHAR(5000),
+	time_stamp DATETIME,
+	foreign key (user_id) references user (user_id)
+);
 
 -- create table job
-create table tb_job (
-	job_id int not null,
-    user_id int not null,
-    job_company_name varchar(100),
-    job_position varchar (100),
-    job_location varchar (100),
-    job_status varchar (100),
-    job_link varchar (100),
-    primary key (job_id)
-	);
+CREATE TABLE job (
+	id INTEGER PRIMARY KEY not null,
+	user_id INTEGER not null,
+	company_name VARCHAR(100),
+	position VARCHAR(100),
+	location VARCHAR(100),
+	status VARCHAR(100),
+	link VARCHAR(100)
+);
 
 -- create table status
-create table tb_status (
-	job_id int not null,
-    status_type varchar (100),
-    status_time_stamp datetime,
-    primary key (job_id)
-	);
+CREATE TABLE status (
+	id INTEGER PRIMARY KEY not null,
+	type VARCHAR(100),
+	time_stamp DATETIME,
+);
 
-    
+-- insert test data for user
+INSERT INTO user (first_name, last_name, email) VALUES 
+	('YIFENG', 'Jin', 'www.111@111.com'),
+	('Niharika', 'Alam', 'www.222@222.com'),
+	('Sandy', 'Qiu', 'www.333@333.com'),
+	('Silvija', 'Skemaite', 'www.444@444.com');
+
+-- insert test data for profile
+INSERT INTO profile (display_name, avatar) VALUES 
+	('AAA', 0),
+	('BBB', 1),
+	('CCC', 0),
+	('DDD', 1);
 	
