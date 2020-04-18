4bbed1edd4bcf114994c461ecd05466d30a02493
SandyQiu <37303878+sqiu001@users.noreply.github.com>
2020-04-18 02:27:37 -0400

    used heidisql for database so different syntax

--- vestigia_heidi.sql
+++ vestigia_heidi.sql
@@ -1,5 +1,5 @@
 -- Dumping database structure for vestigia
-CREATE DATABASE IF NOT EXISTS `vestigia` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE IF NOT EXISTS `vestigia` 
 USE `vestigia`;
 
 -- Dumping structure for table vestigia.tb_follows
@@ -13,7 +13,6 @@ CREATE TABLE IF NOT EXISTS `tb_follows` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Dumping data for table vestigia.tb_follows: ~5 rows (approximately)
-/*!40000 ALTER TABLE `tb_follows` DISABLE KEYS */;
 INSERT INTO `tb_follows` (`user_id`, `followed_user_id`) VALUES
 	(5, 6),
 	(6, 5),
@@ -21,7 +20,6 @@ INSERT INTO `tb_follows` (`user_id`, `followed_user_id`) VALUES
 	(7, 5),
 	(7, 6),
 	(5, 8);
-/*!40000 ALTER TABLE `tb_follows` ENABLE KEYS */;
 
 -- Dumping structure for table vestigia.tb_job
 CREATE TABLE IF NOT EXISTS `tb_job` (
@@ -34,10 +32,6 @@ CREATE TABLE IF NOT EXISTS `tb_job` (
   `job_link` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`job_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Dumping data for table vestigia.tb_job: ~0 rows (approximately)
-/*!40000 ALTER TABLE `tb_job` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tb_job` ENABLE KEYS */;
 
 -- Dumping structure for table vestigia.tb_post
 CREATE TABLE IF NOT EXISTS `tb_post` (
@@ -54,7 +48,6 @@ CREATE TABLE IF NOT EXISTS `tb_post` (
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
 -- Dumping data for table vestigia.tb_post: ~11 rows (approximately)
-/*!40000 ALTER TABLE `tb_post` DISABLE KEYS */;
 INSERT INTO `tb_post` (`post_id`, `post_job_id`, `post_title`, `post_content`, `user_id`, `user_name`, `post_time`) VALUES
 	(1, NULL, 'fdfdfd', 'ffd', 5, 'test', '2020-03-29 15:15:21'),
 	(2, NULL, 'example', 'example post', 6, 'example', '2020-03-29 15:17:24'),
@@ -69,7 +62,6 @@ INSERT INTO `tb_post` (`post_id`, `post_job_id`, `post_title`, `post_content`, `
 	(11, NULL, 'fdsfdsfds', 'dfdsfdsfds', 5, 'test', '2020-04-11 01:11:34'),
 	(12, NULL, 'fdfdfdsfds', 'fdsfdsfdsfd', 5, 'test', '2020-04-11 01:21:17'),
 	(13, NULL, 'dfdsfds', 'fdsfdss', 7, 'sandy', '2020-04-11 01:21:31');
-/*!40000 ALTER TABLE `tb_post` ENABLE KEYS */;
 
 -- Dumping structure for table vestigia.tb_profile
 CREATE TABLE IF NOT EXISTS `tb_profile` (
@@ -79,10 +71,6 @@ CREATE TABLE IF NOT EXISTS `tb_profile` (
   KEY `FK_tb_profile_tb_user` (`user_id`),
   CONSTRAINT `FK_tb_profile_tb_user` FOREIGN KEY (`user_id`) REFERENCES `tb_user` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Dumping data for table vestigia.tb_profile: ~0 rows (approximately)
-/*!40000 ALTER TABLE `tb_profile` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tb_profile` ENABLE KEYS */;
 
 -- Dumping structure for table vestigia.tb_reply
 CREATE TABLE IF NOT EXISTS `tb_reply` (
@@ -100,10 +88,8 @@ CREATE TABLE IF NOT EXISTS `tb_reply` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- Dumping data for table vestigia.tb_reply: ~0 rows (approximately)
-/*!40000 ALTER TABLE `tb_reply` DISABLE KEYS */;
 INSERT INTO `tb_reply` (`reply_id`, `post_id`, `user_id`, `username`, `reply_content`, `reply_time`) VALUES
 	(1, 2, 5, 'test', 'hello how are you?', '2020-03-30 19:48:44');
-/*!40000 ALTER TABLE `tb_reply` ENABLE KEYS */;
 
 -- Dumping structure for table vestigia.tb_status
 CREATE TABLE IF NOT EXISTS `tb_status` (
@@ -112,10 +98,6 @@ CREATE TABLE IF NOT EXISTS `tb_status` (
   `status_time_stamp` datetime DEFAULT NULL,
   PRIMARY KEY (`job_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Dumping data for table vestigia.tb_status: ~0 rows (approximately)
-/*!40000 ALTER TABLE `tb_status` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tb_status` ENABLE KEYS */;
 
 -- Dumping structure for table vestigia.tb_user
 CREATE TABLE IF NOT EXISTS `tb_user` (
@@ -129,15 +111,12 @@ CREATE TABLE IF NOT EXISTS `tb_user` (
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 -- Dumping data for table vestigia.tb_user: ~5 rows (approximately)
-/*!40000 ALTER TABLE `tb_user` DISABLE KEYS */;
 INSERT INTO `tb_user` (`user_id`, `first_name`, `last_name`, `user_name`, `user_password`, `email`) VALUES
 	(5, 'test', 'test', 'test', 'test', 'test@test.com'),
 	(6, 'example', 'example', 'example', 'example', 'example@example.com'),
 	(7, 'sandy', 'qiu', 'sandy', '1', 'sandy@gmail.com'),
 	(8, 'bob', 'builder', 'bob', 'bob', 'bob@pbs.com'),
 	(9, 'star', 'twinkle', 'star', 'star', 'star@star.com');
-/*!40000 ALTER TABLE `tb_user` ENABLE KEYS */;
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+
+
